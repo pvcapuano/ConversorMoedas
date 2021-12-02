25b7e337f78f5fb1fcda1204c9005e6897c4d4da
pvcapuano <pvcapuano@gmail.com>
2021-12-01 23:04:29 -0300

    add modal

--- src/components/Form/Formulario.jsx
+++ src/components/Form/Formulario.jsx
@@ -4,47 +4,84 @@ import {
     Form,
     Col,
     Spinner,
-    Card
+    Card,
+    Alert,
+    Modal
   } from "react-bootstrap"
   import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-  import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons"
+  import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons"
 
 export const Formulario = () => {
     return(
-        <Card style={{marginTop:"40px", width: "100%", height: "300px"}}>
+      <>
+      <Alert variant="danger" show={true} >
+        Tente novamente
+      </Alert>
+        <Card 
+        style={{
+          marginTop:"20px", 
+          width: "60%", 
+          height: "300px",
+          background: "#E9ECEF"
+          }}>
+        
         <Form>
           <Form.Label 
           style={{
             display:"flex", 
             flexDirection:"column", 
-            justifyContent:"center", 
-            alignItems:"center"
+            alignItems:"flex-start",
+            justifyContent:"space-between"
             }}>
-            <Col sm="3" style={{marginTop: "30px"}}>
+            <Col sm="3">
               <Form.Control 
               placeholder="0"
               value={1}
               required
+              style={{width:"200px"}}
               />
             </Col>
-            <Col sm="3" style={{marginTop: "5px"}}>
+            <Col sm="3">
               <Form.Control
               as="select"
+              style={{width:"200px", marginTop:"20px"}}
               />
             </Col>
             <Col sm="1" className="text-center" style={{paddingTop: "5px"}}>
-              <FontAwesomeIcon icon={faAngleDoubleRight} />
+              <FontAwesomeIcon  icon={faAngleDoubleDown}
+              style={{width:"200px", marginTop:"20px"}}
+              />
             </Col>
             <Col sm="3">
-              <Form.Control as="select" />
+              <Form.Control 
+              as="select"
+              style={{width:"200px", marginTop:"20px"}}
+              />
             </Col>
             <Col sm="2">
-              <Button variant="success" type="submit">Converter
+              <Button variant="success" type="submit"
+              style={{width:"200px", marginTop:"20px"}}
+              >Converter
               <Spinner animation="border" size="sm" />
               </Button>
             </Col>
           </Form.Label>
         </Form>
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Conversão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Resultado da conversão
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">
+              Nova conversão
+            </Button>
+          </Modal.Footer>
+        </Modal>
         </Card>
+        
+        </>
     )
 }
